93e142be759861837ccbd7d9a2e0863a0ac85c31
eazytoolhub <eazytoolhub@gmail.com>
2025-04-23 06:19:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello, World1111!"
+    return 'Hello, world!'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
